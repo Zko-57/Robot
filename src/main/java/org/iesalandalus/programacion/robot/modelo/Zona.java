741e8abaacdfd29de54c8bb1448cf8728875cc8a
Zko-57 <adonisdelgado38@gmail.com>
2024-11-28 14:33:21 +0100

    Creado orientacion, coordenada y zona

--- src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
@@ -1,44 +1,49 @@
 package org.iesalandalus.programacion.robot.modelo;
 
+import java.util.Objects;
+
 public record Zona(int ancho, int alto) {
-    public static final int ANCHO_MINIMO = 50;
+    public static final int ANCHO_MINIMO = 10;
     public static final int ANCHO_MAXIMO = 100;
-    public static final int ALTO_MINIMO = 50;
+    public static final int ALTO_MINIMO = 10;
     public static final int ALTO_MAXIMO = 100;
 
 
-    public Zona( int ancho, int alto){
+    public Zona {
+        validarAlto(alto);
+        validarAncho(ancho);
 
     }
 
-    public Zona(){
+    public Zona() { this(ANCHO_MINIMO, ALTO_MINIMO); }
 
+
+
+    private void validarAncho(int ancho) {
+        if (ancho > ANCHO_MAXIMO || ancho < ANCHO_MINIMO) {
+            throw new IllegalArgumentException("Ancho no válido.");
+        }
     }
 
-    private void validarAncho( int ancho){
-
-
-    }
 
     private void validarAlto( int alto){
-
+        if (alto > ALTO_MAXIMO || alto < ALTO_MINIMO) {
+            throw new IllegalArgumentException("Alto no válido.");
+        }
     }
 
-    public Coordenada getCentro(){
 
-    }
+    public Coordenada getCentro() { return new Coordenada(ancho / 2 , alto / 2); }
 
-    public boolean pertenece( Coordenada coordenada){
 
-    }
+    public boolean pertenece(Coordenada coordenada) {
+    Objects.requireNonNull(coordenada, "La coordenada no puede ser nula.");
+    return perteneceX(coordenada.x()) && perteneceY(coordenada.y());
+}
 
-    private boolean perteneceX( int x){
+    private boolean perteneceX( int x) { return (x >= 0) && (x < ancho);}
 
-    }
 
-    private boolean perteneceY( int y){
-
-    }
-
+    private boolean perteneceY( int y) { return (y >= 0) && (y < alto);}
 
 }
